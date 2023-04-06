be9914353d8563c0baf8d344e4d8c8434c5648e2
Devilpacks <mangyr97@mail.ru>
2023-04-06 20:09:49 +0300

    add code to found zeros address

--- src/index.ts
+++ src/index.ts
@@ -48,4 +48,51 @@ async function bootstrap() {
         console.log(error);
     }
 }
-bootstrap()
+
+async function address() {
+    console.log('Starting BRUTER');
+    const options: EthereumOptions = {
+        url: ETH_RPC
+    }
+    const mnemonicProvider = new MnemonicProvider();
+    const ethProvider = new EthereumProvider(options);
+    const phase = PHASE.split(',').map(Number);
+    const maxValue = 2047;
+    const lastIndex = phase.length - 1;
+    let currentCombination = [...phase];
+    try {
+        while (true) {
+            let carry = true;
+            for (let i = lastIndex; i >= 0; i--) {
+                if (carry) {
+                    currentCombination[i]++;
+                    if (currentCombination[i] > maxValue) {
+                        currentCombination[i] = 0;
+                        carry = true;
+                    } else {
+                        carry = false;
+                    }
+                }
+            }
+            if (carry) {
+                break;
+            }
+            console.time('bruter');
+            const mnemonic = mnemonicProvider.getPhrase(currentCombination);
+            const address = ethProvider.addressFromMnemonic(mnemonic);
+            const valid = address.startsWith('0x0000000')
+            console.timeEnd('bruter');
+            if (valid) {
+                console.log(mnemonic);
+                console.log(address);
+                console.log(JSON.stringify(currentCombination));
+                break;
+            }
+            console.log(JSON.stringify(currentCombination));
+        }
+    } catch (error) {
+        console.log(JSON.stringify(currentCombination));
+        console.log(error);
+    }
+}
+address()
